f70871e566e1230e2f30e8b6ba83bde8985ab501
Filippo Vanucci <filippovanucci@C46-Filippo.local>
2018-03-29 20:17:45 -0400

    removed unnecessary fonts

--- aic/aic/Shared/UIFont+AIC.swift
+++ aic/aic/Shared/UIFont+AIC.swift
@@ -12,37 +12,13 @@ extension UIFont {
 	
 	static let aicSectionDescriptionFont: UIFont = UIFont(name: "SabonNextLTPro-Regular", size: 16.0)!
 
-	static let aicNumberPadFont: UIFont = UIFont(name: "SourceSansPro-Regular", size: 30.0)!.upperCaseNumbers()
-
-	static let aicHeaderSmallFont: UIFont = UIFont(name: "SourceSansPro-Black", size: 26.0)!
-
-	static let aicFloorSelectorFont: UIFont = UIFont(name: "SourceSansPro-Regular", size: 21.0)!
-
-//    static let aicTitleFont: UIFont = UIFont(name: "SourceSansPro-Bold", size: 22.0)!
-
-	static let aicItalicTextFont: UIFont = UIFont(name: "Lora-Italic", size: 17.0)!
-
-//	static let aicTextFont: UIFont = UIFont(name: "Lora-Regular", size: 17.0)!
-
-	static let aicShortTextFont: UIFont = UIFont(name: "Lora-Regular", size: 17.0)!
-
-	static let aicSystemTextFont: UIFont = UIFont(name: "SourceSansPro-Regular", size: 17.0)!
-    
-    static let aicDepartmentsFont: UIFont = UIFont(name: "SourceSansPro-Semibold", size: 17.0)!
+	static let aicNumberPadFont: UIFont = UIFont(name: "IdealSans-Book", size: 30.0)!.upperCaseNumbers()
     
     static let aicMapSpacesFont: UIFont = UIFont(name: "IdealSans-Medium", size: 16.0)!.upperCaseNumbers()
 	
 	static let aicMapTextFont: UIFont = UIFont(name: "IdealSans-Medium", size: 15.0)!.upperCaseNumbers()
 	
 	static let aicMapObjectTextFont: UIFont = UIFont(name: "IdealSans-Book", size: 15.0)!.upperCaseNumbers()
-    
-    static let aicMapSVGTextFont: UIFont = UIFont(name: "SourceSansPro-Regular", size: 9.0)!
-    
-    static let aicInstructionsSubtitleFont: UIFont = UIFont(name: "SourceSansPro-Regular", size: 19.0)!
-    
-    static let aicInstructionsTitleFont: UIFont = UIFont(name: "SourceSansPro-Black", size: 30.0)!
-    
-    static let aicMapSVGTextFontFont: UIFont = UIFont(name: "SourceSansPro-Regular", size: 9.0)!
 	
 	static let aicTitleFont: UIFont = UIFont(name: "IdealSans-Medium", size: 21.0)!.upperCaseNumbers()
 	
