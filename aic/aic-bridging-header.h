c4b97bf14346eb8635ce383d138d7c2f9e15decb
Filippo Vanucci <vanuccif@gmail.com>
2019-10-01 16:59:16 -0400

    removed Google Analytics pod

--- aic/aic-bridging-header.h
+++ aic/aic-bridging-header.h
@@ -6,9 +6,4 @@
 #ifndef aic_bridging_header_h
 #define aic_bridging_header_h
 
-#import "GAI.h"
-#import "GAITracker.h"
-#import "GAIFields.h"
-#import "GAIDictionaryBuilder.h"
-
 #endif /* aic_bridging_header_h */
