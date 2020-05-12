8b71b8e2f868937e310fc2a8bc5578641801e1f3
Christopher Luu <nuudles@gmail.com>
2020-05-12 17:07:40 -0400

    Allow selection of new languages (Korean/French)
    This commit also introduces a scroll view for these language selection screens due to the long screens after adding the two new languages.
    
    This commit addresses issue MA-6

--- aic/aic/ViewControllers+Views/Sections/Info/LanguageViewController.swift
+++ aic/aic/ViewControllers+Views/Sections/Info/LanguageViewController.swift
@@ -10,10 +10,14 @@ import UIKit
 import Localize_Swift
 
 class LanguageViewController: UIViewController {
-	let pageView: InfoPageView = InfoPageView()
-	let englishButton: AICButton = AICButton(isSmall: false)
-	let spanishButton: AICButton = AICButton(isSmall: false)
-	let chineseButton: AICButton = AICButton(isSmall: false)
+	private let scrollView = UIScrollView()
+	private let pageView = InfoPageView()
+	private let languageStackView = UIStackView()
+	private let languageButtons: [AICButton] = {
+		Common.Language.allCases.map { (_) in
+			AICButton(isSmall: false)
+		}
+	}()
 
 	init() {
 		super.init(nibName: nil, bundle: nil)
@@ -34,27 +38,37 @@ class LanguageViewController: UIViewController {
 
 		self.view.backgroundColor = .white
 
-		englishButton.setColorMode(colorMode: AICButton.whiteOrangeMode)
-		englishButton.setTitle("English", for: .normal)
-		englishButton.addTarget(self, action: #selector(languageButtonPressed(button:)), for: .touchUpInside)
+		languageStackView.axis = .vertical
+		languageStackView.spacing = 16.0
+		languageStackView.alignment = .center
 
-		spanishButton.setColorMode(colorMode: AICButton.whiteOrangeMode)
-		spanishButton.setTitle("Español", for: .normal)
-		spanishButton.addTarget(self, action: #selector(languageButtonPressed(button:)), for: .touchUpInside)
+		zip(languageButtons, Common.Language.allCases).forEach { (button, language) in
+			switch language {
+			case .english:
+				button.setTitle("English", for: .normal)
+			case .spanish:
+				button.setTitle("Español", for: .normal)
+			case .chinese:
+				button.setTitle("中文", for: .normal)
+			case .korean:
+				button.setTitle("한국어", for: .normal)
+			case .french:
+				button.setTitle("Française", for: .normal)
+			}
 
-		chineseButton.setColorMode(colorMode: AICButton.whiteOrangeMode)
-		chineseButton.setTitle("中文", for: .normal)
-		chineseButton.addTarget(self, action: #selector(languageButtonPressed(button:)), for: .touchUpInside)
+			button.setColorMode(colorMode: AICButton.whiteOrangeMode)
+			button.addTarget(self, action: #selector(languageButtonPressed(button:)), for: .touchUpInside)
+			languageStackView.addArrangedSubview(button)
+		}
 
 		// Add subviews
-		self.view.addSubview(pageView)
-		self.view.addSubview(englishButton)
-		self.view.addSubview(spanishButton)
-		self.view.addSubview(chineseButton)
+		view.addSubview(scrollView)
+		scrollView.addSubview(pageView)
+		scrollView.addSubview(languageStackView)
 
 		let swipeRightGesture: UISwipeGestureRecognizer = UISwipeGestureRecognizer(target: self, action: #selector(swipeRight(recognizer:)))
 		swipeRightGesture.direction = .right
-		self.view.addGestureRecognizer(swipeRightGesture)
+		view.addGestureRecognizer(swipeRightGesture)
 
 		createViewConstraints()
 
@@ -72,43 +86,35 @@ class LanguageViewController: UIViewController {
 	}
 
 	func createViewConstraints() {
-		pageView.autoPinEdge(.top, to: .top, of: self.view)
-		pageView.autoPinEdge(.leading, to: .leading, of: self.view)
-		pageView.autoPinEdge(.trailing, to: .trailing, of: self.view)
+		scrollView.autoPinEdgesToSuperviewEdges()
 
-		englishButton.autoPinEdge(.top, to: .bottom, of: pageView)
-		englishButton.autoAlignAxis(.vertical, toSameAxisOf: self.view)
+		pageView.autoPinEdge(.top, to: .top, of: scrollView)
+		pageView.autoPinEdge(.leading, to: .leading, of: scrollView)
+		pageView.autoPinEdge(.trailing, to: .trailing, of: scrollView)
+		pageView.autoMatch(.width, to: .width, of: scrollView)
 
-		spanishButton.autoPinEdge(.top, to: .bottom, of: englishButton, withOffset: 16)
-		spanishButton.autoAlignAxis(.vertical, toSameAxisOf: self.view)
-
-		chineseButton.autoPinEdge(.top, to: .bottom, of: spanishButton, withOffset: 16)
-		chineseButton.autoAlignAxis(.vertical, toSameAxisOf: self.view)
+		languageStackView.autoPinEdge(.top, to: .bottom, of: pageView)
+		languageStackView.autoAlignAxis(.vertical, toSameAxisOf: scrollView)
+		languageStackView.autoPinEdge(.bottom, to: .bottom, of: scrollView, withOffset: -30)
 	}
 
 	@objc func updateLanguage() {
 		pageView.titleLabel.text = "language_settings_header".localized(using: "LocalizationUI")
 		pageView.textView.text = "language_settings_body".localized(using: "LocalizationUI")
 
-		englishButton.setColorMode(colorMode: AICButton.whiteOrangeMode)
-		spanishButton.setColorMode(colorMode: AICButton.whiteOrangeMode)
-		chineseButton.setColorMode(colorMode: AICButton.whiteOrangeMode)
-		if Localize.currentLanguage() == Common.Language.english.rawValue {
-			englishButton.setColorMode(colorMode: AICButton.orangeMode)
-		} else if Localize.currentLanguage() == Common.Language.spanish.rawValue {
-			spanishButton.setColorMode(colorMode: AICButton.orangeMode)
-		} else if Localize.currentLanguage() == Common.Language.chinese.rawValue {
-			chineseButton.setColorMode(colorMode: AICButton.orangeMode)
+		zip(languageButtons, Common.Language.allCases).forEach { (button, language) in
+			if Localize.currentLanguage() == language.rawValue {
+				button.setColorMode(colorMode: AICButton.orangeMode)
+			} else {
+				button.setColorMode(colorMode: AICButton.whiteOrangeMode)
+			}
 		}
 	}
 
 	@objc func languageButtonPressed(button: UIButton) {
-		if button == englishButton {
-			Localize.setCurrentLanguage(Common.Language.english.rawValue)
-		} else if button == spanishButton {
-			Localize.setCurrentLanguage(Common.Language.spanish.rawValue)
-		} else if button == chineseButton {
-			Localize.setCurrentLanguage(Common.Language.chinese.rawValue)
+		zip(languageButtons, Common.Language.allCases).forEach { (languageButton, language) in
+			guard languageButton == button else { return }
+			Localize.setCurrentLanguage(language.rawValue)
 		}
 
 		// Log analytics
