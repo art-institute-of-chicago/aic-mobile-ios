a1801aeb2767ce637e94c15a5cc11b8ee45cf595
David Bireta <cubefreecoder@gmail.com>
2025-03-12 22:25:55 -0500

    Use the new `title_display` field.

--- aic/aic/Data/AppDataParser.swift
+++ aic/aic/Data/AppDataParser.swift
@@ -1173,7 +1173,7 @@ final class AppDataParser {
 	func parse(eventJson: JSON) throws -> AICEventModel {
         do {
             let eventId = try getString(fromJSON: eventJson, forKey: "id")
-            let title = try getString(fromJSON: eventJson, forKey: "title")
+            let title = try getString(fromJSON: eventJson, forKey: "title_display")
             let longDescription = try getString(fromJSON: eventJson, forKey: "description")
             let shortDescription = try getString(fromJSON: eventJson, forKey: "short_description", optional: true)
             let imageUrl: URL = try getURL(fromJSON: eventJson, forKey: "image_url")!
